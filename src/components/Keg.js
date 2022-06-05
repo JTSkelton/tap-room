d13c084bb6933c8b8782e0a6c4f8afeff0c78cdf
Jack <skelton.jt9@gmail.com>
2022-06-05 14:42:55 -0400

    adding in pint details and increment

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,21 +4,35 @@ import PropTypes from "prop-types";
 function Keg(props) {
   return (
     <React.Fragment>
-      <div class="kegTable" onClick={() => props.whenKegClicked(props.id)}>
-        <table>
-          <th>Name</th>
-          <th>Brand</th>
-          <th>Price</th>
-          <th>Alcohol Content</th>
-          <tbody>
-            <td>{props.name}</td>
-            <td>{props.brand}</td>
-            <td>{props.price}</td>
-            <td>{props.alcoholContent}</td>
-          </tbody>
-        </table>
-        <hr />
-      </div>
+      <table>
+        <th>Name</th>
+        <th>Brand</th>
+        <th>Price</th>
+        <th>Alcohol Content</th>
+        <th>Details</th>
+        <th>Sell A Pint</th>
+        <th>Pints Remaining</th>
+        <th>Pints Sold</th>
+        <tbody>
+          <td>{props.name}</td>
+          <td>{props.brand}</td>
+          <td>{props.price}</td>
+          <td>{props.alcoholContent}</td>
+          <td>
+            <button onClick={() => props.whenKegClicked(props.id)}>
+              Keg Details
+            </button>
+          </td>
+          <td>
+            <button onClick={() => props.whenPintSold(props.pintsRemaining)}>
+              Sell
+            </button>
+          </td>
+          <td>{props.pintsRemaining}</td>
+          <td>{props.pintsSold}</td>
+        </tbody>
+      </table>
+      <hr />
     </React.Fragment>
   );
 }
@@ -28,8 +42,11 @@ Keg.propTypes = {
   brand: PropTypes.string,
   price: PropTypes.string,
   alcoholContent: PropTypes.string,
+  pintsRemaining: PropTypes.number,
+  pintsSold: PropTypes.number,
   id: PropTypes.string,
   whenKegClicked: PropTypes.func,
+  whenPintSold: PropTypes.func,
 };
 
 export default Keg;
